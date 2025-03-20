f66ba1f0d18915c87d673ff131b3662c6c5135a5
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-03-20 11:42:54 +0100

    Added: Dockerfile and Dockercompose + ignore and binded a MongoDB, gitignore, Package*.json's, Backend(live-server) and a Index.html which is empty, i made the whole Application with Live Reloading so in npm run dev, changes to the frontend will be automatically refreshed and i added certain github files for later (ci/cd Pipeline), i intend to use Kubernetes too, i'll have to figure out Kubernetes then when the time is right

---
+++ App/Server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+const dotenv = require("dotenv")
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Live reload setup
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "..", "Client"));
+app.use(connectLivereload());
+
+app.use(express.static(path.join(__dirname, "..", "Client")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "Client", "Views", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Express Server started at http://localhost:${PORT}`);
+});
+
+// Ensure the browser refreshes when files change
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 10);
+});
